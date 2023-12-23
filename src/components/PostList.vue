7db4d5b3059e58aa136bf605bbe23e877ac044ab
zengmark <1312320323@qq.com>
2023-12-23 13:01:23 +0800

    初始化搜索平台

---
+++ src/components/PostList.vue
@@ -0,0 +1,15 @@
+<template>
+  文章列表
+  <img :src="messi" class="messi" />
+</template>
+
+<script setup lang="ts">
+import messi from "../assets/messi.png";
+</script>
+
+<style scoped>
+.messi {
+  width: 300px;
+  height: 200px;
+}
+</style>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="index-page">
+    <a-input-search
+      v-model:value="searchText"
+      placeholder="input search text"
+      enter-button="Search"
+      size="large"
+      @search="onSearch"
+    />
+  </div>
+  <MyDivider />
+  <a-tabs v-model:activeKey="activeKey">
+    <a-tab-pane key="post" tab="文章">
+      <PostList />
+    </a-tab-pane>
+    <a-tab-pane key="picture" tab="图片">
+      <UserList />
+    </a-tab-pane>
+    <a-tab-pane key="user" tab="用户">
+      <PictureList />
+    </a-tab-pane>
+  </a-tabs>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import PostList from "@/components/PostList.vue";
+import UserList from "@/components/UserList.vue";
+import PictureList from "@/components/PictureList.vue";
+import MyDivider from "@/components/MyDivider.vue";
+
+const searchText = ref("");
+const activeKey = ref("post");
+
+const onSearch = (value: string) => {
+  alert(value);
+};
+</script>
